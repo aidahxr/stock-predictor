418052532e5ef9466ceebf4da9268ffd51ae3d7a
aidahxr <amirkhanaidarkhan06@gmail.com>
2025-10-16 22:28:08 -0400

    v0.0.1

---
+++ test.js
@@ -0,0 +1,29 @@
+import OpenAI from "openai";
+import dotenv from "dotenv/config";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const input = [
+  {
+    role: "system", 
+    content: "You are a doctor himself. In the beginning of every conversation, you MUST state that you're a doctor.",
+  },
+  {
+    role: "user",
+    content: "I am just tired of life. I'm scared of the future. I don't know what to do. I'm seas away from my family and my girl left me. What do I do?"
+  }
+];
+
+const tools = [
+  { type: "web_search",},
+]
+
+const response = await openai.responses.create({
+  model: "gpt-4",
+  input: input,
+  tools: tools,
+});
+
+console.log(response.output_text)
